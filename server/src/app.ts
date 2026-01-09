2c9c9c0b8f672c046ef5b95aa399ea37b8f8735c
maxixo <oshodiusman@yahoo.com>
2026-01-09 11:27:39 +0100

    connec frontend with backend

--- server/src/app.ts
+++ server/src/app.ts
@@ -16,7 +16,7 @@ if (isProduction) {
 }
 
 const allowedOrigins = [
-  'https://dataworld-production.up.railway.app',
+  'https://dataworld-server-production.up.railway.app',
   'https://dataworld-xx.vercel.app',
   'https://dataworld-client-fx4l4dktk-maxixos-projects.vercel.app',
 ];
